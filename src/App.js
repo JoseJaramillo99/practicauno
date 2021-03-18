309eeae20e16971820ce929e81d68991f09ea1ed
JoseJaramillo99 <jaramilloj755@gmail.com>
2021-03-18 18:23:15 -0500

    si

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Entra <code>src/App.js</code> aaa.
+          Bienvenido <code>src/App.js</code> Sigmove.
         </p>
         <a
           className="App-link"
